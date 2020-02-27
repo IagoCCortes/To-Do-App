7165b9ca37caf8e9afa75941d7a28c5aa5e07e00
IagoCCortes <iagocortes@gmail.com>
2020-02-27 08:28:48 -0300

    initial server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+app.listen(3000, () => console.log('server started'))
+
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('connected to database'))
+
+mongoose.connect(process.env.DB_Connection_String, {
+    useNewUrlParser: true,
+});
+
+app.listen(3000, () => console.log('server started'))
