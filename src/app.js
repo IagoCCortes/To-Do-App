3b7a59f4b248de3e140f3f5a697149179b0b4fb2
Iago Cortes <iagocortes@gmail.com>
2020-03-09 19:59:45 -0300

    mongo

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'to-do' 
+
+mongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Iago',
+        age: 25
+    })
+})
